45a8e9e5d2929f88a49cfe827ba28fe65d6a3362
Geovan Inacay <geovaninacay@yahoo.com>
2021-09-09 21:55:07 +0100

    Added Initial Customer unit test class
    Added Initial Customer unit test class

---
+++ Customer.API/Customer.HttpClient/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Customer.HttpClient
+{
+    public class Class1
+    {
+    }
+}
---
+++ Customer.API/Customer.Test/CustomerTests.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Customer.API.Controllers;
+using Customer.Business.Customer;
+using Microsoft.Extensions.Configuration;
+using Customer.Repository;
+using System;
+
+namespace Customer.Test
+{
+    [TestClass]
+    public class CustomerTests
+    {
+
+        private IUnitOfWork _unitOfWork;
+        private readonly IConfiguration _configuration;
+
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _unitOfWork = new UnitOfWork("server=(localdb)\\MSSQLLocalDB;database=Customer;Integrated Security=SSPI;");
+
+        }
+
+        [TestMethod]
+        public void GetCustomerFromBusinessLayer()
+        {
+            CustomerBusiness customerBusiness = new CustomerBusiness(_unitOfWork, _configuration);
+
+            var customer = customerBusiness.GetAsync(Guid.NewGuid());
+
+            Assert.IsNotNull(customer);
+
+        }
+    }
+}
